6d03b5c73a4761fc4515457d0f0d761a437aeb20
juandanielgaleano <juandanielgaleano@hotmail.com>
2017-08-01 18:18:23 -0300

    as
    d

---
+++ RecuperatoriosTP/TP 4/Hilo/Descargador.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public delegate void ProgressChanged(int changed);
+    public delegate void ProgressCompleted(string html);
+
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;       
+
+        public event ProgressChanged progChanged; 
+        public event ProgressCompleted progCompleted;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+            this.html = "";
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+
+                cliente.DownloadStringAsync(direccion,html);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progChanged(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.html = e.Result;
+            progCompleted(this.html);
+
+        }
+    }
+}
---
+++ TP 4/Archivos/Texto.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _archivo;
+
+        public Texto(string archivo)
+        {
+            this._archivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+
+            try
+            {
+                StreamWriter escritor = new StreamWriter(this._archivo, true);
+                escritor.Write(datos);
+                escritor.Close();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            try
+            {
+                string line;
+                StreamReader lector = new StreamReader(this._archivo);
+                datos = new List<string>();
+
+                while ((line = lector.ReadLine()) != null)
+                {
+                    datos.Add(line);
+                }
+
+                lector.Close();
+                return true;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }            
+        }
+    }
+}
